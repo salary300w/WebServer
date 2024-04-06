8358e52e27b252339cf37bbf50aeae30205161b4
tengxunyun <tengxunyun>
2024-04-06 14:17:13 +0800

    This version is forked from another open-source library.

---
+++ root/SubPageList.js
@@ -0,0 +1,5 @@
+const articles = [,
+    // { title: "StrategyA", file: "StrategyA/PageMasterStrategyA.html", summary: "主力资金流入排行个股数据！"},
+    { title: "StrategyB", file: "StrategyB/PageMasterStrategyB.html", summary: "盘块、概念主力资金流入数据！"},
+    { title: "StrategyC", file: "StrategyC/PageMasterStrategyC.html", summary: "连板数据获取！"},
+];
