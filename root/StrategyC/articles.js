ae68b5d57df981acb27ad13e12c3c7ad63cab5e4
huaweiyun <huaweiyun@huawei.com>
2025-04-08 16:00:01 +0800

    Tue Apr  8 04:00:01 PM CST 2025

--- root/StrategyC/articles.js
+++ root/StrategyC/articles.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_04_08", file: "2025_04_08.html", summary: "2025_04_08股票数据"},
     { title: "2025_04_07", file: "2025_04_07.html", summary: "2025_04_07股票数据"},
     { title: "2025_04_03", file: "2025_04_03.html", summary: "2025_04_03股票数据"},
     { title: "2025_04_02", file: "2025_04_02.html", summary: "2025_04_02股票数据"},
