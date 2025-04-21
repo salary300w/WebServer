32e83b1d4bf41d40e3ce0cc12257ee3e60b5e5d3
huoshan <huoshan@huoshan.com>
2025-04-21 17:00:01 +0800

    Mon Apr 21 05:00:01 PM CST 2025

--- root/StrategyC/articles.js
+++ root/StrategyC/articles.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_04_21", file: "2025_04_21.html", summary: "2025_04_21股票数据"},
     { title: "2025_04_18", file: "2025_04_18.html", summary: "2025_04_18股票数据"},
     { title: "2025_04_17", file: "2025_04_17.html", summary: "2025_04_17股票数据"},
     { title: "2025_04_16", file: "2025_04_16.html", summary: "2025_04_16股票数据"},
