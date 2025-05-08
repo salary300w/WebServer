1f4f330a4012ca6af5e92ad7ab4e90ae89f638e6
huoshan <huoshan@huoshan.com>
2025-05-08 17:00:01 +0800

    Thu May  8 05:00:01 PM CST 2025

--- root/StrategyC/articles.js
+++ root/StrategyC/articles.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_05_08", file: "2025_05_08.html", summary: "2025_05_08股票数据"},
     { title: "2025_05_07", file: "2025_05_07.html", summary: "2025_05_07股票数据"},
     { title: "2025_05_06", file: "2025_05_06.html", summary: "2025_05_06股票数据"},
     { title: "2025_04_30", file: "2025_04_30.html", summary: "2025_04_30股票数据"},
