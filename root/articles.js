3d9927e58bd0eb8ccd947ebc326eded6db12d25a
JXS <JXS@atm.com>
2025-08-20 21:48:01 +0800

    优化html页面

--- root/articles.js
+++
@@ -1,5 +0,0 @@
-const articles = [,
-    { title: "StrategyA", file: "StrategyA/PageMasterStrategyA.html", summary: "选股策略A"},
-    { title: "StrategyC", file: "StrategyC/PageMasterStrategyC.html", summary: "选股策略C"},
-    // { title: "StrategyB", file: "StrategyB/PageMasterStrategyB.html", summary: "选股策略B"},
-];
