a7c8c7fae4d28a95ce4a0fd5c5be876ce6035259
huoshan <huoshan@huoshan.com>
2025-09-08 19:44:08 +0800

    Mon Sep 8 07:54:23 PM CST 2025

--- filesh/crontableupdate.sh
+++ filesh/crontableupdate.sh
@@ -5,24 +5,25 @@ crontab -r 2>/dev/null
 
 # 定义新任务（可多条）
 TASKS=(
+"43 09 * * 1-5 bash /root/workspace/planb.sh z"
 "50 09 * * 1-5 bash /root/workspace/planb.sh z"
 "20,50 10,11,13,14 * * 1-5 bash /root/workspace/planb.sh z"
 "00 10 * * 1-5 bash /root/workspace/planc.sh z"
-"32 11 * * 1-5 bash /root/workspace/planc.sh b"
+"40 11 * * 1-5 bash /root/workspace/planc.sh b"
 "53 14 * * 1-5 bash /root/workspace/planc.sh b"
-"03 15 * * 1-5 bash /root/workspace/planc.sh l"
-"10 15 * * 1-5 bash /root/workspace/planc.sh u",
-"25 15 * * 1-5 bash /root/workspace/planc.sh c"
+"10 15 * * 1-5 bash /root/workspace/planc.sh l"
+"20 15 * * 1-5 bash /root/workspace/planc.sh u",
+"30 15 * * 1-5 bash /root/workspace/planc.sh c"
 "40 15 * * 1-5 bash /root/workspace/planc.sh y"
 "05 10 * * 1-5 bash /root/workspace/plana.sh z"
-"48 14 * * 1-5 bash /root/workspace/plana.sh b"
-"07 15 * * 1-5 bash /root/workspace/plana.sh l"
-"15 15 * * 1-5 bash /root/workspace/plana.sh u"
-"30 15 * * 1-5 bash /root/workspace/plana.sh c"
-"50 15 * * 1-5 bash /root/workspace/plana.sh y"
-"00 16 * * 1-5 bash /root/workspace/planb.sh z"
-"10 16 * * 1-5 bash /root/workspace/planb.sh b"
-"30 16 * * 1-5 bash /root/workspace/webserver.sh"
+"50 14 * * 1-5 bash /root/workspace/plana.sh b"
+"15 15 * * 1-5 bash /root/workspace/plana.sh l"
+"25 15 * * 1-5 bash /root/workspace/plana.sh u"
+"35 15 * * 1-5 bash /root/workspace/plana.sh c"
+"45 15 * * 1-5 bash /root/workspace/plana.sh y"
+"50 15 * * 1-5 bash /root/workspace/planb.sh z"
+"00 16 * * 1-5 bash /root/workspace/planb.sh b"
+"10 16 * * 1-5 bash /root/workspace/webserver.sh"
 )
 
 # 逐条写入
--- root/StrategyC/SubPageList.js
+++ root/StrategyC/SubPageList.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_09_08", file: "2025_09_08.html", summary: "2025_09_08股票数据"},
     { title: "2025_09_05", file: "2025_09_05.html", summary: "2025_09_05股票数据"},
     { title: "2025_09_04", file: "2025_09_04.html", summary: "2025_09_04股票数据"},
     { title: "2025_09_03", file: "2025_09_03.html", summary: "2025_09_03股票数据"},
