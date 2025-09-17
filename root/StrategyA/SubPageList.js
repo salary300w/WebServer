54a4da54ceec06f7e243a5297bddde65d7f3ff88
huoshan <huoshan@huoshan.com>
2025-09-17 16:00:01 +0800

    Wed Sep 17 04:00:01 PM CST 2025

--- root/StrategyA/SubPageList.js
+++ root/StrategyA/SubPageList.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_09_17", file: "2025_09_17.html", summary: "2025_09_17股票数据"},
     { title: "2025_09_16", file: "2025_09_16.html", summary: "2025_09_16股票数据"},
     { title: "2025_09_15", file: "2025_09_15.html", summary: "2025_09_15股票数据"},
     { title: "2025_09_12", file: "2025_09_12.html", summary: "2025_09_12股票数据"},
