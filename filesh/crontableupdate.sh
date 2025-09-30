e6aed63ae8e8df4ed5ab214fa02696732d3b1611
huoshan <huoshan@huoshan.com>
2025-09-30 16:00:01 +0800

    Tue Sep 30 04:00:01 PM CST 2025

--- filesh/crontableupdate.sh
+++ filesh/crontableupdate.sh
@@ -7,15 +7,12 @@ crontab -r 2>/dev/null
 TASKS=(
 "40,50 09 * * 1-5 bash /root/workspace/planb.sh z"
 "*/10 10,11,13,14 * * 1-5 bash /root/workspace/planb.sh z"
-"05 15 * * 1-5 bash /root/workspace/planc.sh z"
-"10 15 * * 1-5 bash /root/workspace/planc.sh b"
-"15 15 * * 1-5 bash /root/workspace/planc.sh l"
-"20 15 * * 1-5 bash /root/workspace/plana.sh z"
-"25 15 * * 1-5 bash /root/workspace/plana.sh z"
-"30 15 * * 1-5 bash /root/workspace/plana.sh b"
-"35 15 * * 1-5 bash /root/workspace/planb.sh b"
-"40 15 * * 1-5 bash /root/workspace/planb.sh l"
-"00 16 * * 1-5 bash /root/workspace/webserver.sh"
+"05 15 * * 1-5 bash /root/workspace/planb.sh b"
+"10 15 * * 1-5 bash /root/workspace/planb.sh l"
+"15 15 * * 1-5 bash /root/workspace/planc.sh z"
+"20 15 * * 1-5 bash /root/workspace/planc.sh b"
+"25 15 * * 1-5 bash /root/workspace/planc.sh l"
+"30 15 * * 1-5 bash /root/workspace/webserver.sh"
 )
 
 # 逐条写入
--- root/StrategyC/SubPageList.js
+++ root/StrategyC/SubPageList.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2025_09_30", file: "2025_09_30.html", summary: "2025_09_30股票数据"},
     { title: "2025_09_29", file: "2025_09_29.html", summary: "2025_09_29股票数据"},
     { title: "2025_09_26", file: "2025_09_26.html", summary: "2025_09_26股票数据"},
     { title: "2025_09_25", file: "2025_09_25.html", summary: "2025_09_25股票数据"},
