d43ffe512b54b3f7ffe99c32af93475225a73402
huoshan <huoshan@huoshan.com>
2025-12-23 13:35:20 +0800

    Fix typos

--- root/SubPageList.js
+++ root/SubPageList.js
@@ -1,5 +1,5 @@
 const articles = [,
     // { title: "StrategyA", file: "StrategyA/PageMasterStrategyA.html", summary: "主力资金流入排行个股数据！"},
-    { title: "StrategyB", file: "StrategyB/PageMasterStrategyB.html", summary: "盘块、概念主力资金流入数据！"},
-    { title: "StrategyC", file: "StrategyC/PageMasterStrategyC.html", summary: "连板数据获取！"},
+    { title: "StrategyB", file: "StrategyB/PageMasterStrategyB.html", summary: "板块、概念主力资金流入数据！"},
+    { title: "StrategyC", file: "StrategyC/PageMasterStrategyC.html", summary: "沪深主板连板数据！"},
 ];
