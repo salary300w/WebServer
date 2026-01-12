617e9bd72b1f144b9f4484fdb776f84a2fcce38a
huoshan <huoshan@huoshan.com>
2026-01-12 15:30:01 +0800

    Mon Jan 12 03:30:01 PM CST 2026

--- root/StrategyC/SubPageList.js
+++ root/StrategyC/SubPageList.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2026_01_12", file: "2026_01_12.html", summary: "2026_01_12股票数据"},
     { title: "2026_01_09", file: "2026_01_09.html", summary: "2026_01_09股票数据"},
     { title: "2026_01_08", file: "2026_01_08.html", summary: "2026_01_08股票数据"},
     { title: "2026_01_07", file: "2026_01_07.html", summary: "2026_01_07股票数据"},
