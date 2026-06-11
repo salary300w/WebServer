7cb85371b5a3e9e07d6c002a5efe682a2059469b
lighthouse <lighthouse@com>
2026-06-11 15:15:01 +0800

    Thu Jun 11 03:15:01 PM CST 2026

--- root/StrategyB/SubPageList.js
+++ root/StrategyB/SubPageList.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2026_06_11", file: "2026_06_11.html", summary: "2026_06_11股票数据"},
     { title: "2026_06_10", file: "2026_06_10.html", summary: "2026_06_10股票数据"},
     { title: "2026_06_09", file: "2026_06_09.html", summary: "2026_06_09股票数据"},
     { title: "2026_06_08", file: "2026_06_08.html", summary: "2026_06_08股票数据"},
